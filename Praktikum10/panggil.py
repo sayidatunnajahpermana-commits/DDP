0cb03dcc271988cfad19bdd78f351a8a89059ae3
sayyidatunnajahpermana-commits <snajahpermana@gmail.com>
2025-12-03 11:42:04 +0700

    upload file

---
+++ Praktikum10/panggil.py
@@ -0,0 +1,21 @@
+import bangunRuang as br 
+import bangunDatar as bd
+
+print("~~~BANGUN RUANG~~~")
+print(f"Volum kubus dengan sisi 3 adalah {br.kubus(3)}")
+print(f"Volum balok adalah {br.balok(4,5,2)}")
+print(f"Volum prisma segitiga adalah {br.prisma(5,4,5)}")
+print("Volum tabung adalah")
+print("Volum kerucut adalah")
+
+print("~~~BANGUN DATAR~~~")
+print(f"Luas persegi adalah")
+print(f"Luas persegi panjang adalah")
+print(f"Luas segitiga adalah")
+print(f"Luas lingkaran adalah")
+print(f"Luas JajarGenjang adalah")
+print(f"Keliling persegi adalah")
+print(f"Keliling persegi panjang adalah")
+print(f"Keliling segitiga adalah")
+print(f"Keliling lingkaran adalah")
+print(f"Keliling JajarGenjang adalah")
